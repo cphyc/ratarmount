0fbac6380c4e7c54e81b99c0c066d359bc4cd69e
mxmlnkn <mxmlnkn@github.de>
2021-06-28 21:50:50 +0200

    [refactor] Add MountSource abstract base class

--- tests/tests.py
+++ tests/tests.py
@@ -149,7 +149,7 @@ with tempfile.NamedTemporaryFile(suffix=".tar.gz") as tmpTarFile, tempfile.Named
 
         finfo = indexedFile.getFileInfo("/dist/a")
         assert finfo.type == tarfile.DIRTYPE
-        assert indexedFile.getFileInfo("/dist/a", listDir=True) == {
+        assert indexedFile._getFileInfo("/dist/a", listDir=True) == {
             'b': ratarmount.FileInfo(
                 offsetheader=3584,
                 offset=4096,
@@ -164,7 +164,7 @@ with tempfile.NamedTemporaryFile(suffix=".tar.gz") as tmpTarFile, tempfile.Named
                 issparse=0,
             )
         }
-        assert indexedFile.getFileInfo("/", listDir=True) == {
+        assert indexedFile._getFileInfo("/", listDir=True) == {
             'README.md': FileInfo(
                 offsetheader=0,
                 offset=512,
@@ -249,7 +249,7 @@ with tempfile.NamedTemporaryFile(suffix=".gz") as tmpTarFile, tempfile.NamedTemp
 
         expected_name = os.path.basename(tmpTarFile.name)[:-3] if kwargs["fileObject"] is None else "tarFileName"
 
-        finfo = indexedFile.getFileInfo("/", listDir=True)
+        finfo = indexedFile._getFileInfo("/", listDir=True)
         assert expected_name in finfo
         assert finfo[expected_name].size == 11
 
