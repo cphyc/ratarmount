7d2ac2e85c247c3f30bcfd7fdbfad349beb3d69d
mxmlnkn <mxmlnkn@github.de>
2021-07-04 15:29:44 +0200

    [refactor] Use sys.modules instead of globals()

--- ratarmount.py
+++ ratarmount.py
@@ -1550,7 +1550,7 @@ class SQLiteIndexedTar(MountSource):
             if not matches:
                 continue
 
-            if compression.moduleName not in globals() and matches:
+            if compression.moduleName not in sys.modules and matches:
                 return compressionId
 
             try:
@@ -1596,7 +1596,7 @@ class SQLiteIndexedTar(MountSource):
             return fileobj, None, compression, SQLiteIndexedTar._detectTar(fileobj, encoding)
 
         cinfo = supportedCompressions[compression]
-        if cinfo.moduleName not in globals():
+        if cinfo.moduleName not in sys.modules:
             raise CompressionError(
                 "Can't open a {} compressed file '{}' without {} module!".format(
                     compression, fileobj.name, cinfo.moduleName
@@ -2786,7 +2786,7 @@ class TarFileType:
 
             try:
                 # Determining if there are many frames in zstd is O(1) with is_multiframe
-                if compression != 'zst' or supportedCompressions[compression].moduleName not in globals():
+                if compression != 'zst' or supportedCompressions[compression].moduleName not in sys.modules:
                     raise Exception()  # early exit because we catch it ourself anyways
 
                 zstdFile = supportedCompressions[compression].open(fileobj)
@@ -2811,7 +2811,7 @@ class TarFileType:
                 raise argparse.ArgumentTypeError("Archive '{}' can't be opened!\n".format(tarFile))
 
         cinfo = supportedCompressions[compression]
-        if cinfo.moduleName not in globals():
+        if cinfo.moduleName not in sys.modules:
             raise argparse.ArgumentTypeError(
                 "Can't open a {} compressed TAR file '{}' without {} module!".format(
                     compression, fileobj.name, cinfo.moduleName
